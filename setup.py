9cf3dcd209e96ac84ca665dc3dbddd2c00b714bd
Andrey Popp <8mayday@gmail.com>
2012-01-31 23:09:05 +0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = "0.1"
+
+setup(
+    name="routr",
+    version=version,
+    description="URL routing made right",
+    author="Andrey Popp",
+    author_email="8mayday@gmail.com",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    zip_safe=False)
