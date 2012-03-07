b4572bfedb82ef488148ef601da70543fa2228dc
Andrey Popp <8mayday@gmail.com>
2012-03-07 13:01:19 +0400

    routr.resource: clean conventions a bit

--- routr/resource.py
+++ routr/resource.py
@@ -8,38 +8,24 @@
 from routr import route
 from routr import GET, POST, PUT, DELETE, PATCH
 
-__all__ = ("resource", "Resource")
+__all__ = ("resource",)
 
-class Resource(object):
+def resource(res, name=None):
+    name = name or (res.__name__ if hasattr(res, "__name__") else str(res))
+    collection_methods = [route(m, getattr(res, mn), name="%s-%s" % (mn, name))
+        for mn, m in _collection_methods.items() if hasattr(res, mn)]
+    resource_methods = [route(m, getattr(res, mn), name="%s-%s" % (mn, name))
+        for mn, m in _resource_methods.items() if hasattr(res, mn)]
+    return route(*collection_methods, route("{id}", *resource_methods))
 
-    cls = NotImplemented
-    schema = NotImplemented
-    name = NotImplemented
+_collection_methods = {
+    "list": GET,
+    "create": POST
+    }
 
-    def list(self, rng):
-        pass
-
-    def create(self, data):
-        pass
-
-    def get(self, id):
-        pass
-
-    def replace(self, id, data):
-        pass
-
-    def update(self, id, data):
-        pass
-
-    def delete(self, id):
-        pass
-
-def resource(res):
-    return route(
-        route(GET,          res.list,       name="list-%s"      % res.name),
-        route(POST,         res.create,     name="create-%s"    % res.name),
-        route("{id}",
-            route(GET,      res.get,        name="get-%s"       % res.name),
-            route(UPDATE,   res.replace,    name="replace-%s"   % res.name),
-            route(PATCH,    res.update,     name="update-%s"    % res.name),
-            route(DELETE,   res.delete,     name="delete-%s"    % res.name)))
+_resource_methods = {
+    "get": GET,
+    "replace": PUT,
+    "update": PATCH,
+    "delete": DELETE
+    }
