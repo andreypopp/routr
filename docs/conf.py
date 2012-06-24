1f1da962b819495d9433f7b6bb33dbcd2dd27c07
Andrey Popp <8mayday@gmail.com>
2012-06-24 15:13:08 +0400

    docs: simple theme

--- docs/conf.py
+++ docs/conf.py
@@ -94,7 +94,7 @@ pygments_style = 'bw'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'bw'
+html_theme = 'simple'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -105,7 +105,7 @@ html_theme_options = {
     }
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [pkg_resources.resource_filename('bw_sphinxtheme', 'themes')]
+html_theme_path = [pkg_resources.resource_filename('sphinxtheme_simple', 'themes')]
 
 
 # The name for this set of Sphinx documents.  If None, it defaults to
