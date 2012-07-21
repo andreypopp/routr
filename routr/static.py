baadc7f08823086f85fc9b9a7db945653bfd18fa
Andrey Popp <8mayday@gmail.com>
2012-07-21 20:53:59 +0400

    routr.static: fix

--- routr/static.py
+++ routr/static.py
@@ -8,17 +8,21 @@
 
 """
 
+from os.path import join
 from webob.static import FileApp
 from routr import route, GET
 
-__all__ = ("static", "static_view")
+__all__ = ("static",)
 
 def static(prefix, directory):
     """ Define a route which serves static files"""
     if prefix.endswith("/"):
         prefix = prefix[:-1]
-    return route(GET, "%s/{path:path}" % prefix, static_view)
+    return route(GET, "%s/{path:path}" % prefix, make_static_view(directory))
 
-def static_view(request, path):
-    """ View for serving static files"""
-    return FileApp(path)(request)
+def make_static_view(directory):
+    def static_view(request, path):
+        """ View for serving static files"""
+        return FileApp(join(directory, path))(request)
+    static_view.static_view = True
+    return static_view
