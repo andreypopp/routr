50da86f747d794c85d020fd7798e82a4bdc500b8
Andrey Popp <8mayday@gmail.com>
2012-10-23 02:53:35 +0400

    routr.static.static: accept annotations and return Response subclass

--- routr/static.py
+++ routr/static.py
@@ -9,12 +9,21 @@
 """
 
 from os.path import join
+from webob import Response
 from webob.static import FileApp
 from routr import route, GET
 
 __all__ = ('static',)
 
-def static(prefix, directory):
+class _ForceResponse(Response):
+
+    def __init__(self, underlying):
+        self.underlying = underlying
+
+    def __call__(self, environ, start_response):
+        return self.underlying(environ, start_response)
+
+def static(prefix, directory, **kw):
     """ Define a route which serves static assets
 
     :param prefix:
@@ -24,13 +33,14 @@ def static(prefix, directory):
     """
     if prefix.endswith('/'):
         prefix = prefix[:-1]
+    kw['static_view'] = True
     return route(
         GET, '%s/{path:path}' % prefix,
-        make_static_view(directory), static_view=True)
+        make_static_view(directory), **kw)
 
 def make_static_view(directory):
     def static_view(request, path):
         """ View for serving static files"""
-        return FileApp(join(directory, path))(request)
+        return _ForceResponse(FileApp(join(directory, path))(request))
     static_view.static_view = True # b/c
     return static_view
